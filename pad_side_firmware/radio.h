a541ad1c077857a6111257082b168736cffb76f4
Logan Wright <logan_wright5@icloud.com>
2025-01-19 23:46:36 -0700

    Duplicated source files for message structs (overtly simple message structures) and radio manager class. Will be converting structs to classes and will use inheritance for more capable protocol structure

---
+++ pad_side_firmware/radio.h
@@ -0,0 +1,23 @@
+#ifndef RADIO_H
+#define RADIO_H
+
+#include "Arduino.h"
+#include "SPI.h"
+#include "RF24.h"
+#include "nRF24L01.h"
+
+#include "messages.h"
+
+class CommHandler {
+public:
+  CommHandler(uint8_t ce, uint8_t csn);
+  ~CommHandler() { delete _radio; };
+
+  void initialize(byte srcAddress[6] = "00001", byte destAddress[6] = "00002");
+  bool sendPacket(command_t& command);
+
+private:
+  RF24* _radio;
+};
+
+#endif
